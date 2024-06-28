d32fe578cf44c5ded0f939dd018f2ace3d70faef
Jonah Nichols <jonah@Jonahs-MacBook-Pro.local>
2024-06-28 11:24:53 -0400

    Add margins

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import logo from "../assets/tmp.jpg"
 
 export const Home: React.FC = () =>
 {
@@ -21,7 +21,7 @@ export const Home: React.FC = () =>
         },
         {
             n: 4,
-            title: "Attending to Linguisitc and Culture",
+            title: "Attending to Linguistic and Culture",
             description: "Explain what is it and why it is important"
         }
     ]
@@ -32,16 +32,16 @@ export const Home: React.FC = () =>
         // Goals
 
         // Goal info
-        <div className="flex w-full h-full flex-col">
+        <div className="flex w-full h-full flex-col mb-20">
             <div className="flex w-full h-30">
                 <div className="w-35 h-full">
-                    <img></img>
+                    <img src={logo} alt="Read Play Math Logo" className="w-full h-full"></img>
                 </div>
-                <div className="w-65 h-full">
-
+                <div className="w-65 h-full flex items-center justify-center text-center mx-20 my-5">
+                    <p className="text-2xl text-center">Read. Play. Math. Is an online learning platform designed to help children strengthen their math skills by ...</p>
                 </div>
             </div>
-            <div className="flex w-full h-70">{/* Goals */}
+            <div className="flex w-full h-70 mt-5">{/* Goals */}
                 {goalInfo.map((goal) => (
                     <Goal n={goal.n} title={goal.title} description={goal.description}></Goal>
                 ))}
@@ -62,11 +62,11 @@ const Goal: React.FC<GoalProps> = ({n, title, description}) =>
 {
 
     return(
-        <div className="flex flex-col ">
-            <h4>{n}</h4>
+        <div className="flex flex-col border-r-2 border-black border-solid flex-1 items-center p-5">
+            <h1 className="text-center">{n}</h1>
             <div className="flex flex-col gap-4">
-                <p>{title}</p>
-                <p>{description}</p>
+                <h4 className="text-center">{title}</h4>
+                <p className="text-center">{description}</p>
             </div>
         </div>
     )
