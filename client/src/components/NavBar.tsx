9f8eb4a05536f5ec9d50532b6ac0ccfbdd7648c0
Jonah Nichols <jonah@Jonahs-MacBook-Pro.local>
2024-06-28 11:05:56 -0400

    Fix location and create elements

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { SearchBar } from "./SearchBar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
@@ -11,12 +11,33 @@ interface DropDownItem
 interface DropDownProps
 {
     name: string,
-    items: DropDownItem[]
+    items: DropDownItem[],
+    show: boolean,
+    hideF: () => void,
+    showF: () => void,
 };
 
 export const NavBar: React.FC = () =>
 {
     const buttonStyle: string = "h-70 w-fit mx-4"
+    const [showAbout, setShowAbout] = useState(false);
+
+    const hideAbout = () => {
+        setShowAbout(false);
+    }
+
+    const showAboutF = () => {
+        setShowAbout(true);
+    }
+    const [showHowToUse, setShowHowToUse] = useState(false);
+
+    const hideHowToUse = () => {
+        setShowHowToUse(false);
+    }
+
+    const showHowToUseF = () => {
+        setShowHowToUse(true);
+    }
     const dropDownInfo: DropDownProps[] = [
         {
             name: "About",
@@ -29,7 +50,10 @@ export const NavBar: React.FC = () =>
                     name: "Our Team",
                     link: "/about/our-team"
                 }
-            ]
+            ],
+            show: showAbout,
+            showF: showAboutF,
+            hideF: hideAbout,
         },
         {
             name: "How To Use",
@@ -46,21 +70,24 @@ export const NavBar: React.FC = () =>
                     name: "Progress Monitoring",
                     link: "/how-to-use/progress-monitoring"
                 }
-            ]
+            ],
+            show: showHowToUse,
+            showF: showHowToUseF,
+            hideF: hideHowToUse,
         }
     ]
 
     return (
         // RPL logo --> Home --> Search Bar --> About (Dropdown) --> How to Use (Dropdown) --> Books --> FAQ
-        <div className="h-10 w-full bg-blue-300">
-            <div>
+        <div className="sticky top-0 h-20 w-full bg-blue-300 flex justify-evenly items-center gap-4">
+            <div className="flex gap-4 items-center justify-items-center">
                 <a className="" href="/">Home</a>
                 <SearchBar></SearchBar>
             </div>
-            <div>
+            <div className="flex gap-4">
                 {
                     dropDownInfo.map((props) => (
-                        <DropDown name={props.name} items={props.items}></DropDown>
+                        <DropDown name={props.name} items={props.items} hideF={props.hideF} showF={props.showF} show={props.show}></DropDown>
                     ))
                 }
                 <a className={buttonStyle} href="/books">Books</a>
@@ -70,15 +97,16 @@ export const NavBar: React.FC = () =>
     )
 };
 
-export const DropDown: React.FC<DropDownProps> = ({name, items}) =>
+export const DropDown: React.FC<DropDownProps> = ({name, items, show, showF, hideF}) =>
 {
 
     return (
-        <div>
+        <div onMouseEnter={showF} onMouseLeave={hideF} className="px-2 py-2">
             <NavDropdown
               id="nav-dropdown-dark-example"
               title={name}
               menuVariant="dark"
+              show={show}
             >
             {items.map((item) => (
                 <NavDropdown.Item href={item.link}>{item.name}</NavDropdown.Item>
