a43ede5b09c65d4f243019cd0efa98e42d605f8c
Jonah Nichols <jonah@Jonahs-MacBook-Pro.local>
2024-06-28 15:02:30 -0400

    Added icon to props

--- client/src/components/RouteButton.tsx
+++ client/src/components/RouteButton.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { IconType } from "react-icons";
 
 export interface RouteButtonProps {
     title: string,
-    route: string
+    route: string,
+    Icon: React.FC | IconType
 };
 
-export const RouteButton: React.FC<RouteButtonProps> = ({title, route}) => {
+export const RouteButton: React.FC<RouteButtonProps> = ({title, route, Icon}) => {
     return (
         <Link to={route} className="block">
             <button
@@ -14,21 +16,7 @@ export const RouteButton: React.FC<RouteButtonProps> = ({title, route}) => {
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
                 {title}
-                <svg
-                    className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 10"
-                >
-                    <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M1 5h12m0 0L9 1m4 4L9 9"
-                    />
-                </svg>
+                <Icon className="ml-4"></Icon>
             </button>
         </Link>
     )
