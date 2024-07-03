a6b037119030918006a14867d4be77012360103c
Jonah Nichols <jonah@Jonahs-MacBook-Pro.local>
2024-07-03 16:38:29 -0400

    Add external option for external links

--- client/src/components/RouteButton.tsx
+++ client/src/components/RouteButton.tsx
@@ -5,19 +5,32 @@ import { IconType } from "react-icons";
 export interface RouteButtonProps {
     title: string,
     route: string,
-    Icon: React.FC | IconType
+    Icon: React.FC | IconType,
+    external?: boolean
 };
 
-export const RouteButton: React.FC<RouteButtonProps> = ({title, route, Icon}) => {
+export const RouteButton: React.FC<RouteButtonProps> = ({title, route, Icon, external}) => {
+    const buttonContent = (
+        <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+            {title}
+            <Icon className="ml-4" />
+        </button>
+    );
+
     return (
-        <Link to={route} className="block">
-            <button
-                type="button"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-                {title}
-                <Icon className="ml-4"></Icon>
-            </button>
-        </Link>
-    )
+        <>
+            {external ? (
+                <a href={route} target="_blank" rel="noopener noreferrer" className="block">
+                    {buttonContent}
+                </a>
+            ) : (
+                <Link to={route} className="block">
+                    {buttonContent}
+                </Link>
+            )}
+        </>
+    );
 } 
