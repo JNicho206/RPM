9e344214a43163c32074c72b6f5269cb0c7e5795
Jonah Nichols <jonah@Jonahs-MacBook-Pro.local>
2024-07-03 16:35:29 -0400

    Barebones section content

--- client/src/components/AdviceEntry.tsx
+++ client/src/components/AdviceEntry.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 
 export interface AdviceEntryProps {
-
+    name: string,
+    advice: string
 };
 
-export const AdviceEntry: React.FC<AdviceEntryProps> = ({}) => {
+export const AdviceEntry: React.FC<AdviceEntryProps> = ({name, advice}) => {
 
     return (
         <div>
-            
+            <p>{advice}</p>
+            <span> --- {name}</span>
         </div>
     )
 }
