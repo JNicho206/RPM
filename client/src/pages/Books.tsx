12439b7b7cb5d2171c082b1c20bf0e27ed8a3227
Jonah Nichols <jonah@Jonahs-MacBook-Pro.local>
2024-07-03 16:39:00 -0400

    Setup book entries

--- client/src/pages/Books.tsx
+++ client/src/pages/Books.tsx
@@ -1,29 +1,24 @@
 import React from "react";
 import { BookEntry, BookEntryProps } from "../components/BookEntry";
+import bookData from "../assets/data/books/books.json";
+
+const images = import.meta.glob('../assets/data/books/images/*.{jpg,png,jpeg,gif}');
+const imgPaths = Object.keys(images).map((img: string) => {return img.replace("../assets", "src/assets")})
 
 
 export const Books: React.FC = () =>
 {
-    const books: BookEntryProps[] = [
-        {
-            title: "OMW Pilar solves problems",
-            description: "This book focuses on Pause"
-        },
-        {
-            title: "OMW Pilar solves problems",
-            description: "This book focuses on Pause"
-        },
-        {
-            title: "OMW Pilar solves problems",
-            description: "This book focuses on Pause"
-        }
-    ];
+
+
 
     return (
-        <div>
-            {books.map((book) => (
-                <BookEntry title={book.title} description={book.description}></BookEntry>
-            ))}
+        <div className="flex flex-col gap-4 items-center mx-96 my-20">
+            <h1 className="section-header">Books</h1>
+            <div className="flex flex-col gap-16 items-center">
+                {bookData.map((book, index) => (
+                    <BookEntry key={index} title={book.title} description={book.description} img={imgPaths.find((b: string) => {return b.endsWith(book.img)})} link={book.link}></BookEntry>
+                ))}
+            </div>
         </div>
     )
 }
