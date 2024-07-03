ab7fb2bb0fda19209c8fa182429d902f4f6e87b9
Jonah Nichols <jonah@Jonahs-MacBook-Pro.local>
2024-07-03 16:39:28 -0400

    Add advice and faq entries

--- client/src/pages/FAQ.tsx
+++ client/src/pages/FAQ.tsx
@@ -10,20 +10,20 @@ export const FAQ: React.FC = () =>
 
     return (
         <div className="flex flex-col items-center mx-20 my-20 gap-10">
-            <div className="flex flex-col">
-                <h1 className="text-center border-b-4 border-black border-solid w-min px-8">FAQ</h1>
-                <div>
+            <div className="flex flex-col items-center">
+                <h1 className="section-header">FAQ</h1>
+                <div className="flex flex-col items-center gap-4">
                     {questions.map((q) => (
-                        <FAQEntry></FAQEntry>
+                        <FAQEntry question="How do I use this website?" answer="Check out our how to use page. This page covers materials you may need, and best practices."></FAQEntry>
                     ))}
                 </div>
             </div>
 
             <div>
-                <h1 className="border-b-4 border-black border-solid w-fit px-8">Advice From Teachers</h1>
-                <div>
+                <h1 className="section-header">Advice From Teachers</h1>
+                <div className="flex flex-col gap-4 items-center">
                     {advice.map((a) => (
-                        <AdviceEntry></AdviceEntry>
+                        <AdviceEntry name={a.name} advice={a.advice}></AdviceEntry>
                     ))}
                 </div>
             </div>
