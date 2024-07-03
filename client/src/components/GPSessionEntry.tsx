985d99b0e2ae486d41506e646b6a6144cc81f17e
Jonah Nichols <jonah@Jonahs-MacBook-Pro.local>
2024-07-03 16:37:49 -0400

    Fill in for initial implementation

---
+++ client/src/components/GPSessionEntry.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { FaFilePdf } from "react-icons/fa6";
+import Accordion from "react-bootstrap/Accordion";
+import { RouteButton } from "./RouteButton";
+
+export interface GPSessionEntryProps {
+    name: string,
+    pdf?: string,
+    objective: string
+};
+
+
+export const GPSessionEntry: React.FC<GPSessionEntryProps> = ({name, pdf, objective}) => {
+
+    const pdfRoute = "/how-to-use/guided-play/" + pdf;
+
+    return (
+        <div>
+            <Accordion>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>{name}</Accordion.Header>
+                    <Accordion.Body>
+                        <div className="flex flex-col items-center gap-2">
+                            <span>{objective}</span>
+                            <RouteButton title="Session Guide" route={pdfRoute} Icon={FaFilePdf}></RouteButton>
+                        </div>
+
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+        </div>
+
+    )
+
+
+};
+
+
+
+
---
+++ client/src/components/GPUnitEntry.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Accordion from "react-bootstrap/Accordion";
+import { GPSessionEntry, GPSessionEntryProps } from "./GPSessionEntry";
+
+
+export interface GPUnitEntryProps {
+    name: string,
+    sessions: GPSessionEntryProps[],
+};
+
+export const GPUnitEntry: React.FC<GPUnitEntryProps> = ({name, sessions}) => {
+
+    return (
+        <div>
+            <Accordion className="w-[700px]">
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>{name}</Accordion.Header>
+                    <Accordion.Body>
+                        {sessions.map((s, index) => (
+                            <GPSessionEntry name={s.name} objective={s.objective}></GPSessionEntry>
+                        ))}
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+        </div>
+
+    )
+};
