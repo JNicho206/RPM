4cf51f29635bce80fb4c137ad3cc34c2501a9557
Jonah Nichols <jonah@Jonahs-MacBook-Pro.local>
2024-08-05 11:48:30 -0400

    Change interface

--- client/src/components/ToggleButtonGroup.tsx
+++ client/src/components/ToggleButtonGroup.tsx
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 export interface ToggleButton {
-    text: string,
-    toggled: boolean,
-    onClick: () => void;
+    id: number;
+    text: string;
+    toggled: boolean;
+    onClick: (id: number) => void;
 }
 
 export interface ToggleButtonGroupProps {
-    buttons: ToggleButton[],
-    multiSelect: boolean,
+    buttons: ToggleButton[];
+    multiSelect: boolean;
 }
 
-export const ToggleButtonGroup: React.FC<ToggleButtonGroupProps> = ({buttons, multiSelect}) => {
+export const ToggleButtonGroup: React.FC<ToggleButtonGroupProps> = ({ buttons, multiSelect }) => {
+    const [toggledButtons, setToggledButtons] = useState<ToggleButton[]>(buttons);
+
+    const handleButtonClick = (id: number) => {
+        buttons[id].onClick(id);
+        setToggledButtons(prevState => {
+            return prevState.map(button => {
+                if (button.id === id) {
+                    if (multiSelect) {
+                        return { ...button, toggled: !button.toggled };
+                    } else {
+                        return { ...button, toggled: true };
+                    }
+                } else {
+                    return multiSelect ? button : { ...button, toggled: false };
+                }
+            });
+        });
+    };
 
     return (
         <div className="inline-flex">
-            {buttons.map((button) => (
-                <button onClick={button.onClick} className={`px-4 py-2 rounded-md ${button.toggled ? "bg-blue-500 text-white" : "bg-gray-200 text-gray-800"}`}>
+            {toggledButtons.map((button, i) => (
+                <button
+                    key={i}
+                    onClick={() => handleButtonClick(button.id)}
+                    className={`px-4 py-2 rounded-md ${button.toggled ? "bg-blue-500 text-white" : "bg-gray-200 text-gray-800"}`}
+                >
                     {button.text}
                 </button>
             ))}
-            
         </div>
     );
 };
